df9bc9f1e14141ae5eb46b8e77deb19575c8da69
amiradp <amiradp3@gmail.com>
2023-08-19 21:24:23 +0330

    add authall and cheange setting for allauth

--- accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('signup/', views.signupUpView.as_view(), name='signup'),
-]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-from django.views.generic import CreateView
-from django.urls import reverse_lazy
-
-from .forms import CustomUserCreationForm
-
-class signupUpView(CreateView):
-    form_class = CustomUserCreationForm
-    template_name = 'registration/signup.html'
-    success_url = reverse_lazy('login')
+# from django.shortcuts import render
+# from django.views.generic import CreateView
+# from django.urls import reverse_lazy
+#
+# from .forms import CustomUserCreationForm
+#
+# class signupUpView(CreateView):
+#     form_class = CustomUserCreationForm
+#     template_name = 'registration/signup.html'
+#     success_url = reverse_lazy('login')
