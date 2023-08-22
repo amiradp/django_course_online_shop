cae010963a19ecfe3da22b09c1ad13b1a3ce69d6
amiradp <amiradp3@gmail.com>
2023-08-22 20:03:30 +0330

    add detail view and pk

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='ProductListView'),
+    path('<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -10,4 +10,7 @@ class ProductListView(generic.ListView):
     context_object_name = 'products'
 
 
-
+class ProductDetailView(generic.DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
